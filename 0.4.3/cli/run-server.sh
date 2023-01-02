42708c35e3e10997c7fee2f814b1a728953d9bde
Lavender Shannon <retrodaredevil@gmail.com>
2023-01-01 19:51:34 -0600

    Removed comments

--- 0.4.3/cli/run-server.sh
+++ 0.4.3/cli/run-server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Assume working directory is /app
-# Data files are in config
-# Autosaves are in save/autosave/
-
-
 save_file=$(ls -t /app/game/save/autosave/* 2>/dev/null | head -n1)
 
 if [ ! -f "$save_file" ]; then
@@ -12,7 +7,5 @@ if [ ! -f "$save_file" ]; then
   exit 1
 fi
 
-#openrct2-cli scan-objects --openrct2-data-path="/app/config" --user-data-path="/app/game"
-#openrct2-cli host "$save_file" --openrct2-data-path="/app/config" --user-data-path="/app/game" $@
 openrct2-cli scan-objects --user-data-path="/app/game"
 openrct2-cli host "$save_file" --user-data-path="/app/game" $@
